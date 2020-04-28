3ba4e4582ec034057401089044b257d7b83560c3
William Wang <william.wang10@student.tdsb.on.ca>
2020-04-28 16:06:47 -0400

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+import java.util.*;
+/**
+ * Write a description of class Interface here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Interface
+{
+    public static void main(String[] args) {
+
+    }
+}
